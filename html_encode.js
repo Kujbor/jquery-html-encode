71a4dc43aab5a1a27b9e84097e6ab6e48b353f9d
Kujbor <Kujbor@ya.ru>
2013-11-19 17:08:08 +0300

    Update html_encode.js

--- html_encode.js
+++ html_encode.js
@@ -1,18 +1,18 @@
 /*
- * html_encode.js - jQuery plugin for encode/decode html entities
+ * jquery.encode.js - jQuery plugin for encode/decode html entities
  * Author Oleg Taranov aka Kujbor
  * Copyright (C) 2013: CubeComp Development
  */
-(function($) {
+define("jquery.encode", ["jquery"], function($) {
 
     "use strict";
 
     $.htmlEncode = function(data) {
-        return $('<div />').text(data).html().replace(/"/g, '&quot;');
+        return $("<div />").text(data).html().replace(/"/g, "&quot;");
     };
 
     $.htmlDecode = function(data) {
-        return $('<div />').html(data.replace(/&quot;/g, '"')).text();
+        return $("<div />").html(data.replace(/&quot;/g, '"')).text();
     };
 
-})(jQuery);
+});
